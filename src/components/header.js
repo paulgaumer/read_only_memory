db9f69723a33cdc82de7c75da0401f2a686514ba
Paul Gaumer <mitsudomoe8@gmail.com>
2020-02-14 15:27:29 +0100

    Add dynamic arrow in header on current location

--- src/components/header.js
+++ src/components/header.js
@@ -1,50 +1,46 @@
 import React from "react"
 import { Link } from "gatsby"
 
+const NavListItem = ({ location, category }) => {
+  // Clean accents as the slug doesn't contain any
+  const categorySlug =
+    category === "éditeurs" ? category.replace("é", "e") : category
+  // Clean accents as the location slug doesn't contain any
+  const categoryName =
+    category === "éditeurs" ? category.replace("é", "e") : category
+  // Check if the current location url matches the category
+  const isCurrentLocation = location.pathname === `/${categoryName}`
+
+  return (
+    <li
+      className={`${
+        isCurrentLocation ? "text-myGrey-primary" : ""
+      } uppercase pr-5`}
+    >
+      <Link to={`/${categorySlug}`} className="flex items-center">
+        <span className={`${isCurrentLocation ? "inline-block" : "hidden"}`}>
+          ►
+        </span>
+        {/* Get rid of the final "s" */}
+        <span className="uppercase">{category.slice(0, -1)}</span>
+      </Link>
+    </li>
+  )
+}
+
 const Header = ({ location }) => {
+  const navList = ["titres", "auteurs", "éditeurs", "collections"]
+
   return (
     <header className="fixed top-0 w-full">
-      <div
-        // style={{
-        //   margin: `0 auto`,
-        //   maxWidth: 960,
-        //   padding: `1.45rem 1.0875rem`,
-        // }}
-        className="flex justify-between pt-2 pb-0 px-4 text-5xl"
-      >
-        <ul className="flex">
-          <li
-            className={`${
-              location.pathname === "/titres" ? "text-myGrey-primary" : ""
-            } uppercase pr-5`}
-          >
-            <Link to="/titres">titre</Link>
-          </li>
-
-          <li
-            className={`${
-              location.pathname === "/auteurs" ? "text-myGrey-primary" : ""
-            } uppercase pr-5`}
-          >
-            <Link to="/auteurs">auteur</Link>
-          </li>
-
-          <li
-            className={`${
-              location.pathname === "/editeurs" ? "text-myGrey-primary" : ""
-            } uppercase pr-5`}
-          >
-            <Link to="/editeurs">éditeur</Link>
-          </li>
-
-          <li
-            className={`${
-              location.pathname === "/collections" ? "text-myGrey-primary" : ""
-            } uppercase`}
-          >
-            <Link to="/collections">collection</Link>
-          </li>
-        </ul>
+      <div className="flex justify-between pt-2 pb-0 px-4 text-5xl">
+        <div className="categories">
+          <ul className="flex">
+            {navList.map(category => (
+              <NavListItem location={location} category={category} />
+            ))}
+          </ul>
+        </div>
         <span className="uppercase">
           <Link to="/a-propos/">à propos</Link>
         </span>

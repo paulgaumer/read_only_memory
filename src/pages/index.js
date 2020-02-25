287d88a6c3cd8395244d51f49c78fc759bc5574f
Paul Gaumer <mitsudomoe8@gmail.com>
2020-02-25 22:48:45 +0100

    Animation still not working on Safari, back to basic solution

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import useDarkMode from "use-dark-mode"
@@ -6,49 +6,11 @@ import SEO from "../components/seo"
 import "../styles/global.scss"
 
 const Container = styled.div`
-  /* min-height: -webkit-fill-available;
-  min-height: 100vh; */
-
-  @-webkit-keyframes slideInFromTop {
-    0% {
-      -webkit-height: 0px;
-    }
-    50% {
-      -webkit-height: 500px;
-    }
-    100% {
-      -webkit-height: ${props => props.windowHeight + "px"};
-    }
-  }
-
-  @keyframes slideInFromTop {
-    0% {
-      height: 0px;
-    }
-    100% {
-      height: ${props => props.windowHeight + "px"};
-    }
-  }
-
-  -webkit-animation-name: slideInFromTop;
-  -webkit-animation-duration: 1.5s;
-  -webkit-animation-timing-function: ease-out;
-  -webkit-animation-delay: 0s;
-  -webkit-animation-iteration-count: 1;
-  -webkit-animation-fill-mode: forwards;
-  animation: slideInFromTop 1.5s ease-out 0s 1 forwards;
+  min-height: 100vh;
+  min-height: -webkit-fill-available;
 `
 
 const IndexPage = () => {
-  // Use JS to handle window height for mobile browsers
-  const [windowHeight, setWindowHeight] = useState("")
-  useEffect(() => {
-    // Check for the presence of window. Needed for SSR.
-    if (typeof window !== undefined) {
-      setWindowHeight(window.innerHeight)
-    }
-  }, [])
-
   // Setup the switch between dark & light mode
   const darkMode = useDarkMode(false)
   const handleClick = mode => {
@@ -58,11 +20,7 @@ const IndexPage = () => {
   return (
     <>
       <SEO title="Home" />
-      <Container
-        className="flex flex-col text-3xl lg:text-4xl xl:text-5xl"
-        // style={{ height: `${windowHeight}px` }}
-        windowHeight={windowHeight}
-      >
+      <Container className="flex flex-col text-3xl lg:text-4xl xl:text-5xl">
         <Link
           to="/titres"
           className="flex-auto block flex flex-col"

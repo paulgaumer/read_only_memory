9764b5f287a1954ec54d0416b5fddecae452aa2d
Paul Gaumer <mitsudomoe8@gmail.com>
2020-03-04 14:58:58 +0100

    Add accordions to other index pages

--- src/components/lists/list-item-others-mobile.js
+++ src/components/lists/list-item-others-mobile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { Link } from "gatsby"
 import {
   AccordionItem,
@@ -27,43 +27,64 @@ const ListItemOthers = ({ item, page, location }) => {
   const categories = ["titles", "authors", "editors", "collections"]
 
   return (
-    <AccordionItem uuid={item.name}>
-      <AccordionItemHeading>
-        <AccordionItemButton className="outline-none">
-          <AccordionItemState>
-            {state => {
-              return (
-                <div className="flex justify-between text-primary">
-                  <span>{capitalize(item.name)}</span>
-                  {state.expanded ? (
-                    <span className="text-primary">&#9660;</span>
-                  ) : (
-                    <span className="text-secondary">&#9654;</span>
-                  )}
-                </div>
-              )
-            }}
-          </AccordionItemState>
-        </AccordionItemButton>
-      </AccordionItemHeading>
-      <AccordionItemPanel>
-        {item.titles.map((title, i) => {
-          if (category !== page) {
-            return (
-              <div key={category} className="text-primary">
-                <ItemCategory
-                  categoryInfo={title[category]}
-                  name={category}
-                  title={title.name}
-                  key={`${title.id}-${category}`}
-                />
-              </div>
-            )
-          }
-          return null
-        })}
-      </AccordionItemPanel>
-    </AccordionItem>
+    <div id={item.name}>
+      {item.titles.map((title, i) => {
+        return (
+          <AccordionItem
+            uuid={`${item.name}-${i}`}
+            className="border-b border-myGrey-secondary"
+          >
+            <AccordionItemHeading>
+              <AccordionItemButton className="outline-none">
+                <AccordionItemState>
+                  {state => {
+                    return (
+                      <div className="px-4 flex justify-between">
+                        {state.expanded ? (
+                          <span className="text-primary">
+                            {capitalize(item.name)}
+                          </span>
+                        ) : (
+                          <span>{capitalize(item.name)}</span>
+                        )}
+
+                        {state.expanded ? (
+                          <span className="text-primary">&#9660;</span>
+                        ) : (
+                          <span className="text-secondary">&#9654;</span>
+                        )}
+                      </div>
+                    )
+                  }}
+                </AccordionItemState>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              {categories.map(category => {
+                if (category !== page) {
+                  return (
+                    <Link
+                      to={`${sanitizeSlug(title.slug)}`}
+                      state={{ prevPath: location.pathname }}
+                    >
+                      <div key={category} className="text-primary">
+                        <ItemCategory
+                          categoryInfo={title[category]}
+                          name={category}
+                          title={title.name}
+                          key={`${title.id}-${category}`}
+                        />
+                      </div>
+                    </Link>
+                  )
+                }
+                return null
+              })}
+            </AccordionItemPanel>
+          </AccordionItem>
+        )
+      })}
+    </div>
   )
 }
 
--- src/components/lists/list-item-titles-mobile.js
+++ src/components/lists/list-item-titles-mobile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { Link } from "gatsby"
 import { sanitizeSlug, capitalize } from "../../utils/utils"
 import {

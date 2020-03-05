69897d02e008d46e057312398fa5bf09543c1421
Paul Gaumer <mitsudomoe8@gmail.com>
2020-03-05 17:20:31 +0100

    Fix build issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,9 @@
 // Allows us to keep the state given by React Context on page change
-import React from "react"
+
+// import React from "react"
 import GlobalContextProvider from "./src/context/global-context-provider"
+const React = require("react")
+// const GlobalContextProvider = require("./src/context/global-context-provider")
 
 export const wrapRootElement = ({ element }) => {
   return <GlobalContextProvider>{element}</GlobalContextProvider>
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,7 @@
 // Allows us to keep the state given by React Context on page change
 const React = require("react")
 const GlobalContextProvider = require("./src/context/global-context-provider")
+  .default
 
 exports.wrapRootElement = ({ element }) => {
   return <GlobalContextProvider>{element}</GlobalContextProvider>

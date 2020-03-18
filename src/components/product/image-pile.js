5781fee88d7fc28cb343497a0fd2ebbda8e35f87
Paul Gaumer <mitsudomoe8@gmail.com>
2020-03-18 10:33:21 +0100

    Change top offset and click on previous picture

--- src/components/product/image-pile.js
+++ src/components/product/image-pile.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react"
-import { array } from "prop-types"
 
 const ImagePile = ({ images }) => {
   const [visiblePic, setVisiblePic] = useState(1)
 
-  function handleClick(picId, e) {
+  function handleClick(pictureId, e) {
     console.log(e.target)
-    console.log(picId)
-    if (picId === images.length) {
+    console.log(pictureId)
+    if (pictureId === images.length) {
+      // RESET
       setVisiblePic(1)
+    } else if (pictureId < visiblePic) {
+      // COME BACK TO PREVIOUS PICTURE
+      setVisiblePic(pictureId)
     } else {
-      setVisiblePic(picId + 1)
+      // GO TO NEXT PICTURE
+      setVisiblePic(pictureId + 1)
     }
   }
 
@@ -31,14 +35,14 @@ const ImagePile = ({ images }) => {
           left = "50%"
           transform = "translate(-50%)"
         } else if (i % 2 !== 0) {
-          top = `${i * 50}px`
+          top = `${i * 20}px`
           left = "20px"
         } else {
-          top = `${i * 50}px`
+          top = `${i * 20}px`
           right = "20px"
         }
 
-        const picId = i + 1
+        const pictureId = i + 1
 
         const style = {
           maxWidth: "80%",
@@ -47,8 +51,8 @@ const ImagePile = ({ images }) => {
           bottom,
           right,
           transform,
-          zIndex: visiblePic === picId ? "45" : picId,
-          visibility: picId <= visiblePic ? "visible" : "hidden",
+          zIndex: visiblePic === pictureId ? "45" : pictureId,
+          visibility: pictureId <= visiblePic ? "visible" : "hidden",
         }
 
         return (
@@ -58,7 +62,8 @@ const ImagePile = ({ images }) => {
             alt=""
             className={`absolute`}
             style={style}
-            onClick={e => handleClick(picId, e)}
+            onClick={e => handleClick(pictureId, e)}
+            onKeyDown={e => handleClick(pictureId, e)}
           />
         )
       })}
